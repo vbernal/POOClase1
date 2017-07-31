2e84a468b67c0c9541177853250c6f9bda28f4f2
Estudiante <Estudiante@10.203.3.215>
2017-07-31 16:13:09 -0500

    Version 0 Ejemplo Hola Mundo

---
+++ src/HolaMundo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class HolaMundo {
+    public static void main(String[] arg){
+        int a = 8;
+        for(int i=0; i<a; i++){
+        System.out.println("Hola Mundo");
+        }
+    }
+};
